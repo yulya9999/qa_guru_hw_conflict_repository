d5530cbdcdeb9f3fe027bf4c345952b1e3cbcf7e
Yulia Vdovina <yulya_vdovina_2000@mail.ru>
2024-05-01 21:38:46 +0300

    com -> ru

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from selene import browser, be, have
 
 def test_search_google():
 
-    browser.open('https://google.com')
+    browser.open('https://google.ru')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
