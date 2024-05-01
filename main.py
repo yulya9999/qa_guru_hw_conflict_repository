6d9c637bd8b08b3f30d6a669804704cdb8095967
Yulia Vdovina <yulya_vdovina_2000@mail.ru>
2024-05-01 21:54:05 +0300

    заменен url

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from selene import browser, be, have
 
 def test_search_google():
 
-    browser.open('htttttttttttps://google.ru')
+    browser.open('https://yandex.ru')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
